f5158a6c7d35db46a4c70e6bd0eca21539438edd
Malte <maltewollermann@yahoo.de>
2021-06-22 04:21:29 +0200

    Update VideoCabinInstructions.py

--- VideoCabinInstructions.py
+++ VideoCabinInstructions.py
@@ -1,34 +1,42 @@
 # tkinter for GUI
-from tkinter import *
-#from tkinter import filedialog
-#from tkinter import messagebox
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
 # glob for finding files
-#import glob
+import glob
 
 # subprocess and os for operating system operations
-#import subprocess
-#import os
+import subprocess
+import os
 
 # shutil for copying files between partitions
-#from shutil import copyfile
+from shutil import copyfile
 
 # datetime and re for creating files and directories with dates and times
-#from datetime import datetime
+from datetime import datetime
+
+# for screen information
+import screeninfo
 
 # for window positioning
-windowWidth = 640
-windowHeight = 1080
-windowPositionX = 1920 - windowWidth
+desktopResolutionX = 1920
+desktopResolutionY = 1080
+
+windowWidth = int(desktopResolutionX / 2.5)
+windowHeight = desktopResolutionY
+
+windowPositionX = 0 - windowWidth
 windowPositionY = 0
 
 def introWindow():
-    windowNew = Tk()
-    #windowNew.geometry(str(windowWidth) + "x" + str(windowHeight) + "+" + str(windowPositionX) + "+" + str(windowPositionY))
+    windowNew = tk.Tk()
+    windowNew.geometry(str(windowWidth) + "x" + str(windowHeight) + "+" + str(windowPositionX) + "+" + str(windowPositionY))
     windowNew.title = "Instructions"
 
-    windowNew.mainloop
+    windowNew.mainloop()
 
+print(screeninfo.get_monitors())
 introWindow()
 
 # https://stackoverflow.com/questions/53541138/how-to-add-multiline-text-dynamically-in-tkinter-python
