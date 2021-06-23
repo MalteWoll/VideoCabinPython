1154b4895643ab22aea7b214d35339b655dedb7c
Malte <maltewollermann@yahoo.de>
2021-06-23 11:19:02 +0200

    Update VideoCabinInstructions.py

--- VideoCabinInstructions.py
+++ VideoCabinInstructions.py
@@ -39,23 +39,49 @@ imageFilesDir = "C:/Users/malte/Dropbox/Studium/PAM3/guiImages"
 
 # Image files
 audioLevelImg = imageFilesDir + "/audioLevel.JPG"
+audioKnob = imageFilesDir + "/audioPlaceholder.jpg"
 
 # Some parameters for formatting text
 labelWrapLength = windowWidth-100
-paddingX = 20
-paddingY = 20
+paddingX = 10
+paddingY = 10
 textFontStyle = "Helvetica"
 textFontSize = 20
+textFontSizeSmaller = 16
 
 textWelcomeMessage1 = "Welcome to the Video Cabin! Please follow the instructions to learn how to create your videos."
 textWelcomeMessage2 = "Press the 'Continue' button to follow the instructions. If you are already familiar with the application, you can skip this tutorial by pressing the 'Skip' button."
 
 textAudioInstructions1 = "We need to make sure your voice can be heard, so let's first calibrate the audio signal."
 textAudioInstructions2 = "To do so, please speak in a volume as if you were recording, and stand where you would stand."
-textAudioInstructions3 = "While speaking, please observe the audio gauge (??) on the bottom left side of this monitor. The audio level should be within the green area while you are speaking, as shown below."
+textAudioInstructions3 = "While speaking, please observe the audio gauge (??) on the bottom left side of this monitor. The audio level should be within the green area while you are speaking, roughly in the middle of the bar, as shown below."
+textAudioInstructions4 = "If the audio level is green, great! You can continue. If not, look at the rotary knob on the device in front of you, which is pictured below."
+textAudioInstructions5 = "If your recorded audio was too low (the bar was too far on the left side), slightly turn the know to the right, and try speaking again. If it was too high (the bar was in the yellow or red), slightly turn the knob to the left, and try speaking again."
 
 textButtonContinue = "Continue"
 textButtonSkip = "Skip"
+textButtonBack = "Back"
+
+# Get the size of an image and dynamically resize it to fit on the GUI window.
+# Since most images have a greater x-value, it should be enough to scale on that axis. TODO: If not, first check which side is larger.
+def resizeImg(img, resizeFactor):
+    # Find the factor the images needs to be scaled with to fit into the window
+    scalingFactor = (img.size[0] / windowWidth) * resizeFactor
+
+    # Calculate the new values of the window, depending on the previous found factor
+    newSizeX = (img.size[0] / scalingFactor)
+    newSizeY = (img.size[1] / scalingFactor)
+
+    # Reduce the size of the image by another value, to leave some room at the sides of the GUI window
+    newSizeX = int(newSizeX * 0.9)
+    newSizeY = int(newSizeY * 0.9)
+
+    # Debug
+    #print(str(newSizeX) + ", " + str(newSizeY))
+
+    imgResize = img.resize((newSizeX, newSizeY), Image.ANTIALIAS)
+
+    return imgResize
 
 # Window for reviewing the last clip recorded and to delete it, if needed
 def fileControl(windowOld):
@@ -69,9 +95,9 @@ def audioCalibration(windowOld):
     
     windowNew = tk.Tk()
     windowNew.geometry(str(windowWidth) + "x" + str(windowHeight) + "+" + str(windowPositionX) + "+" + str(windowPositionY))
-    windowNew.title = "Audio calibration"
+    windowNew.title("Audio calibration")
 
-    textFont = (textFontStyle,textFontSize)
+    textFont = (textFontStyle,textFontSizeSmaller)
     text_audio1 = Label(windowNew, text=textAudioInstructions1, font=textFont, wraplength=labelWrapLength )
     text_audio1.grid(row=0, padx=paddingX, pady=paddingY)
     text_audio2 = Label(windowNew, text=textAudioInstructions2, font=textFont, wraplength=labelWrapLength )
@@ -79,7 +105,26 @@ def audioCalibration(windowOld):
     text_audio3 = Label(windowNew, text=textAudioInstructions3, font=textFont, wraplength=labelWrapLength )
     text_audio3.grid(row=2, padx=paddingX, pady=paddingY)
 
-    img = Image.open(audioLevelImg)
+    # Load the image and resize it
+    img = resizeImg(Image.open(audioLevelImg), 1)
+    img = ImageTk.PhotoImage(img)
+
+    labelAudioLevelImg = Label(windowNew, image=img).grid(row=3, padx=paddingX, pady=paddingY)
+
+    text_audio4 = Label(windowNew, text=textAudioInstructions4, font=textFont, wraplength=labelWrapLength )
+    text_audio4.grid(row=4, padx=paddingX, pady=paddingY)
+
+    # TODO: Add pictures
+    img2 = resizeImg(Image.open(audioKnob), 2)
+    img2 = ImageTk.PhotoImage(img2)
+
+    labelAudioKnow = Label(windowNew, image=img2).grid(row=5, padx=paddingX, pady=paddingY)
+
+    text_audio5 = Label(windowNew, text=textAudioInstructions5, font=textFont, wraplength=labelWrapLength )
+    text_audio5.grid(row=6, padx=paddingX, pady=paddingY)
+
+    button_continue = Button(windowNew, text=textButtonContinue, font=textFont).grid(row=7, padx=paddingX, pady=paddingY)
+    button_back = Button(windowNew, text=textButtonBack, font=textFont, command= lambda: introWindow(windowNew)).grid(row=8, padx=paddingX, pady=paddingY)
 
     windowNew.mainloop()
 
@@ -90,8 +135,8 @@ def introWindow(windowOld):
         windowOld.destroy()
 
     windowNew = tk.Tk()
+    windowNew.title("Instructions")
     windowNew.geometry(str(windowWidth) + "x" + str(windowHeight) + "+" + str(windowPositionX) + "+" + str(windowPositionY))
-    windowNew.title = "Instructions"
 
     textFont = (textFontStyle,textFontSize)
 
@@ -110,4 +155,10 @@ def introWindow(windowOld):
     windowNew.mainloop()
 
 # Script starts here
-introWindow(None)
+#introWindow(None)
+
+def main():
+  print ("Hello World!")
+  
+if __name__== "__main__":
+  main()
